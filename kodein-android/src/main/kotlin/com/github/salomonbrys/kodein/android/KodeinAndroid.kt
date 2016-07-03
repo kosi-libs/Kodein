02223caea34eb431cefac399dbb09845acac40cf
Salomon BRYS <salomon.brys@gmail.com>
2016-07-03 15:02:40 +0200

    Use of `KodeinAware` instead of `KodeinApplication`

--- kodein-android/src/main/kotlin/com/github/salomonbrys/kodein/android/KodeinAndroid.kt
+++ kodein-android/src/main/kotlin/com/github/salomonbrys/kodein/android/KodeinAndroid.kt
@@ -6,51 +6,42 @@ import android.content.AbstractThreadedSyncAdapter
 import android.content.Context
 import android.content.Loader
 import android.view.View
-import com.github.salomonbrys.kodein.Kodein
-import com.github.salomonbrys.kodein.KodeinInjected
-import com.github.salomonbrys.kodein.LazyKodein
-import com.github.salomonbrys.kodein.with
+import com.github.salomonbrys.kodein.*
 
 
-interface KodeinApplication {
-    val kodein: Kodein
-}
+fun Context.lazyKodeinFromApp() = LazyKodein(lazy { (applicationContext as KodeinAware).kodein })
 
+fun Fragment.lazyKodeinFromApp() = LazyKodein(lazy { (activity.applicationContext as KodeinAware).kodein })
 
+fun android.support.v4.app.Fragment.lazyKodeinFromApp() = LazyKodein(lazy { (activity.applicationContext as KodeinAware).kodein })
 
-fun Context.lazyKodeinFromApp() = LazyKodein(lazy { (applicationContext as KodeinApplication).kodein })
+fun Dialog.lazyKodeinFromApp() = LazyKodein(lazy { (context.applicationContext as KodeinAware).kodein })
 
-fun Fragment.lazyKodeinFromApp() = LazyKodein(lazy { (activity.applicationContext as KodeinApplication).kodein })
+fun View.lazyKodeinFromApp() = LazyKodein(lazy { (context.applicationContext as KodeinAware).kodein })
 
-fun android.support.v4.app.Fragment.lazyKodeinFromApp() = LazyKodein(lazy { (activity.applicationContext as KodeinApplication).kodein })
+fun AbstractThreadedSyncAdapter.lazyKodeinFromApp() = LazyKodein(lazy { (context.applicationContext as KodeinAware).kodein })
 
-fun Dialog.lazyKodeinFromApp() = LazyKodein(lazy { (context.applicationContext as KodeinApplication).kodein })
+fun Loader<*>.lazyKodeinFromApp() = LazyKodein(lazy { (context.applicationContext as KodeinAware).kodein })
 
-fun View.lazyKodeinFromApp() = LazyKodein(lazy { (context.applicationContext as KodeinApplication).kodein })
-
-fun AbstractThreadedSyncAdapter.lazyKodeinFromApp() = LazyKodein(lazy { (context.applicationContext as KodeinApplication).kodein })
-
-fun Loader<*>.lazyKodeinFromApp() = LazyKodein(lazy { (context.applicationContext as KodeinApplication).kodein })
-
-fun android.support.v4.content.Loader<*>.lazyKodeinFromApp() = LazyKodein(lazy { (context.applicationContext as KodeinApplication).kodein })
+fun android.support.v4.content.Loader<*>.lazyKodeinFromApp() = LazyKodein(lazy { (context.applicationContext as KodeinAware).kodein })
 
 
 
-val Context.appKodein: () -> Kodein get() = { (applicationContext as KodeinApplication).kodein }
+val Context.appKodein: () -> Kodein get() = { (applicationContext as KodeinAware).kodein }
 
-val Fragment.appKodein: () -> Kodein get() = { (activity.applicationContext as KodeinApplication).kodein }
+val Fragment.appKodein: () -> Kodein get() = { (activity.applicationContext as KodeinAware).kodein }
 
-val android.support.v4.app.Fragment.appKodein: () -> Kodein get() = { (activity.applicationContext as KodeinApplication).kodein }
+val android.support.v4.app.Fragment.appKodein: () -> Kodein get() = { (activity.applicationContext as KodeinAware).kodein }
 
-val Dialog.appKodein: () -> Kodein get() = { (context.applicationContext as KodeinApplication).kodein }
+val Dialog.appKodein: () -> Kodein get() = { (context.applicationContext as KodeinAware).kodein }
 
-val View.appKodein: () -> Kodein get() = { (context.applicationContext as KodeinApplication).kodein }
+val View.appKodein: () -> Kodein get() = { (context.applicationContext as KodeinAware).kodein }
 
-val AbstractThreadedSyncAdapter.appKodein: () -> Kodein get() = { (context.applicationContext as KodeinApplication).kodein }
+val AbstractThreadedSyncAdapter.appKodein: () -> Kodein get() = { (context.applicationContext as KodeinAware).kodein }
 
-val Loader<*>.appKodein: () -> Kodein get() = { (context.applicationContext as KodeinApplication).kodein }
+val Loader<*>.appKodein: () -> Kodein get() = { (context.applicationContext as KodeinAware).kodein }
 
-val android.support.v4.content.Loader<*>.appKodein: () -> Kodein get() = { (context.applicationContext as KodeinApplication).kodein }
+val android.support.v4.content.Loader<*>.appKodein: () -> Kodein get() = { (context.applicationContext as KodeinAware).kodein }
 
 
 
