f7de81335a99d9c09c04bef32bfbfe03aff7f4b6
Salomon BRYS <salomon.brys@gmail.com>
2016-12-29 17:41:41 +0100

    All Android services are now bound

--- kodein-android/src/main/java/com/github/salomonbrys/kodein/android/AndroidModule.kt
+++ kodein-android/src/main/java/com/github/salomonbrys/kodein/android/AndroidModule.kt
@@ -3,33 +3,51 @@ package com.github.salomonbrys.kodein.android
 import android.accounts.AccountManager
 import android.app.*
 import android.app.admin.DevicePolicyManager
-import android.content.ClipboardManager
-import android.content.ContentResolver
-import android.content.Context
-import android.content.SharedPreferences
+import android.app.job.JobScheduler
+import android.app.usage.NetworkStatsManager
+import android.app.usage.UsageStatsManager
+import android.appwidget.AppWidgetManager
+import android.bluetooth.BluetoothManager
+import android.content.*
 import android.content.pm.ApplicationInfo
+import android.content.pm.LauncherApps
 import android.content.pm.PackageManager
+import android.content.pm.ShortcutManager
 import android.content.res.AssetManager
 import android.content.res.Resources
+import android.hardware.ConsumerIrManager
 import android.hardware.SensorManager
+import android.hardware.camera2.CameraManager
+import android.hardware.display.DisplayManager
+import android.hardware.fingerprint.FingerprintManager
+import android.hardware.input.InputManager
 import android.hardware.usb.UsbManager
 import android.location.LocationManager
 import android.media.AudioManager
+import android.media.MediaRouter
+import android.media.midi.MidiManager
+import android.media.projection.MediaProjectionManager
+import android.media.session.MediaSessionManager
+import android.media.tv.TvInputManager
 import android.net.ConnectivityManager
+import android.net.nsd.NsdManager
 import android.net.wifi.WifiManager
 import android.net.wifi.p2p.WifiP2pManager
 import android.nfc.NfcManager
-import android.os.DropBoxManager
-import android.os.Looper
-import android.os.PowerManager
-import android.os.Vibrator
+import android.os.*
+import android.os.health.SystemHealthManager
 import android.os.storage.StorageManager
 import android.preference.PreferenceManager
+import android.print.PrintManager
 import android.service.wallpaper.WallpaperService
+import android.telecom.TelecomManager
+import android.telephony.CarrierConfigManager
+import android.telephony.SubscriptionManager
 import android.telephony.TelephonyManager
 import android.view.LayoutInflater
 import android.view.WindowManager
 import android.view.accessibility.AccessibilityManager
+import android.view.accessibility.CaptioningManager
 import android.view.inputmethod.InputMethodManager
 import android.view.textservice.TextServicesManager
 import com.github.salomonbrys.kodein.Kodein
@@ -102,6 +120,61 @@ val androidModule = Kodein.Module {
     bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.WIFI_SERVICE) as WifiManager }
     bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.WINDOW_SERVICE) as WindowManager }
 
+    if (Build.VERSION.SDK_INT >= 16) {
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.INPUT_SERVICE) as InputManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.MEDIA_ROUTER_SERVICE) as MediaRouter }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.NSD_SERVICE) as NsdManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= 17) {
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.USER_SERVICE) as UserManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= 18) {
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= 19) {
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.CAPTIONING_SERVICE) as CaptioningManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.CONSUMER_IR_SERVICE) as ConsumerIrManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.PRINT_SERVICE) as PrintManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= 21) {
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.APPWIDGET_SERVICE) as AppWidgetManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.BATTERY_SERVICE) as BatteryManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.CAMERA_SERVICE) as CameraManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.MEDIA_SESSION_SERVICE) as MediaSessionManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.TELECOM_SERVICE) as TelecomManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.TV_INPUT_SERVICE) as TvInputManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= 22) {
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= 23) {
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.CARRIER_CONFIG_SERVICE) as CarrierConfigManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.FINGERPRINT_SERVICE) as FingerprintManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.MIDI_SERVICE) as MidiManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= 24) {
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.HARDWARE_PROPERTIES_SERVICE) as HardwarePropertiesManager }
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.SYSTEM_HEALTH_SERVICE) as SystemHealthManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= 25) {
+        bindDirect() from erasedFactory { ctx: Context -> ctx.getSystemService(Context.SHORTCUT_SERVICE) as ShortcutManager }
+    }
 }
 
 /**
@@ -174,4 +247,59 @@ fun autoAndroidModule(app: Application) = Kodein.Module {
     bindDirect() from erasedProvider { ctx().getSystemService(Context.WIFI_SERVICE) as WifiManager }
     bindDirect() from erasedProvider { ctx().getSystemService(Context.WINDOW_SERVICE) as WindowManager }
 
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.INPUT_SERVICE) as InputManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.MEDIA_ROUTER_SERVICE) as MediaRouter }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.NSD_SERVICE) as NsdManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.DISPLAY_SERVICE) as DisplayManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.USER_SERVICE) as UserManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.CAPTIONING_SERVICE) as CaptioningManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.CONSUMER_IR_SERVICE) as ConsumerIrManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.PRINT_SERVICE) as PrintManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.APPWIDGET_SERVICE) as AppWidgetManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.BATTERY_SERVICE) as BatteryManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.CAMERA_SERVICE) as CameraManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.MEDIA_SESSION_SERVICE) as MediaSessionManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.TELECOM_SERVICE) as TelecomManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.TV_INPUT_SERVICE) as TvInputManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.CARRIER_CONFIG_SERVICE) as CarrierConfigManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.FINGERPRINT_SERVICE) as FingerprintManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.MIDI_SERVICE) as MidiManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.HARDWARE_PROPERTIES_SERVICE) as HardwarePropertiesManager }
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.SYSTEM_HEALTH_SERVICE) as SystemHealthManager }
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+        bindDirect() from erasedProvider { ctx().getSystemService(Context.SHORTCUT_SERVICE) as ShortcutManager }
+    }
 }
