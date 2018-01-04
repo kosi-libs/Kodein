7725c57bb927f4edc90aec0a956eb8aa34dbfc9d
Salomon BRYS <salomon.brys@gmail.com>
2018-01-04 16:56:16 +0100

    compile fix

--- demo/demo-android/src/main/java/kodein/demo/DemoApplication.kt
+++ demo/demo-android/src/main/java/kodein/demo/DemoApplication.kt
@@ -1,13 +1,13 @@
 package kodein.demo
 
+import android.app.Activity
 import android.app.Application
 import kodein.demo.coffee.Coffee
 import kodein.demo.coffee.Kettle
 import kodein.demo.coffee.electricHeaterModule
-import kodein.demo.coffee.thermosiphonModule
 import org.kodein.*
-import org.kodein.android.ActivityScope
 import org.kodein.android.androidModule
+import org.kodein.android.androidScope
 
 class DemoApplication : Application(), KodeinAware {
 
@@ -21,7 +21,7 @@ class DemoApplication : Application(), KodeinAware {
         bind<Coffee>() with provider { Coffee(instance()) }
 
         // this is bound in the scope of an activity so any retrieval using the same activity will return the same Kettle instance
-        bind<Kettle<Coffee>>() with scoped(ActivityScope).singleton { Kettle<Coffee>(instance(), instance(), instance(), provider()) }
+        bind<Kettle<Coffee>>() with scoped(androidScope<Activity>()).singleton { Kettle<Coffee>(instance(), instance(), instance(), provider()) }
     }
 
 }
