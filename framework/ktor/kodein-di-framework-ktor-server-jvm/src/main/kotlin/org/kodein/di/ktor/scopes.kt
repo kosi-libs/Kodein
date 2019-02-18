36f7a6e9e67029d7dbe73352ee21879e31674847
Romain Bsl <romain.boisselle@protonmail.com>
2019-02-18 11:20:38 +0100

    create SessionScope to provide Kodein scope upon Ktor Sessions

--- framework/ktor/kodein-di-framework-ktor-server-jvm/src/main/kotlin/org/kodein/di/ktor/scopes.kt
+++ framework/ktor/kodein-di-framework-ktor-server-jvm/src/main/kotlin/org/kodein/di/ktor/scopes.kt
@@ -1,8 +1,34 @@
 package org.kodein.di.ktor
 
+import org.kodein.di.bindings.Scope
+import org.kodein.di.bindings.ScopeRegistry
+import org.kodein.di.bindings.StandardScopeRegistry
+
 /**
  * Interface that will help leverage the use of Kodein in the Ktor [Sessions] context
  */
 interface KtorSession {
     fun getSessionId(): Any
+}
+
+/**
+ * Kodein scope that will provide singletons according to a specific [KtorSession]
+ */
+object SessionScope : Scope<KtorSession> {
+
+    private val mapRegistry = HashMap<Any, ScopeRegistry>()
+
+    /**
+     * Reclaim the right [ScopeRegistry] regarding to the given [KtorSession]
+     * This will help maintaining and retrieving singletons linked with the [KtorSession]
+     */
+    override fun getRegistry(context: KtorSession): ScopeRegistry {
+        return synchronized(mapRegistry) {
+            mapRegistry[context.getSessionId()] ?: run {
+                val scopeRegistry = StandardScopeRegistry()
+                mapRegistry[context.getSessionId()] = scopeRegistry
+                scopeRegistry
+            }
+        }
+    }
 }
