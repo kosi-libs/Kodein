8a6a534244ccd878a5e563141bebb708d4f7215f
Romain Bsl <romain.boisselle@protonmail.com>
2019-02-18 11:17:21 +0100

    create KtorSession interface to represent a Session across Ktor server when using Kodein

--- framework/ktor/kodein-di-framework-ktor-server-jvm/src/main/kotlin/org/kodein/di/ktor/scopes.kt
+++ framework/ktor/kodein-di-framework-ktor-server-jvm/src/main/kotlin/org/kodein/di/ktor/scopes.kt
@@ -1,2 +1,8 @@
 package org.kodein.di.ktor
 
+/**
+ * Interface that will help leverage the use of Kodein in the Ktor [Sessions] context
+ */
+interface KtorSession {
+    fun getSessionId(): Any
+}
