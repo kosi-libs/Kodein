b8a3bb7536512e7c4d70c4b504bea69dc9bf5d8f
Romain Bsl <romain.boisselle@protonmail.com>
2019-02-20 11:42:22 +0100

    defining extension function upon Ktor main objects to retrieve a global Kodein container that have been configured by the KodeinFeature

--- framework/ktor/kodein-di-framework-ktor-server-jvm/src/main/kotlin/org/kodein/di/ktor/closest.kt
+++ framework/ktor/kodein-di-framework-ktor-server-jvm/src/main/kotlin/org/kodein/di/ktor/closest.kt
@@ -1,2 +1,43 @@
 package org.kodein.di.ktor
 
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.routing.Route
+import io.ktor.routing.Routing
+import io.ktor.util.AttributeKey
+import io.ktor.util.pipeline.PipelineContext
+import org.kodein.di.Kodein
+
+
+// attribute key for storing injector in a call
+val KodeinKey = AttributeKey<Kodein>("kodein")
+
+/**
+ * Getting the global [Kodein] container from the [Application]
+ */
+fun Application.kodein() = attributes[KodeinKey]
+
+/**
+ * Getting the global [Kodein] container from the [ApplicationCall]
+ */
+fun PipelineContext<*, ApplicationCall>.kodein() = context.application.kodein()
+
+/**
+ * Getting the global [Kodein] container from the [Routing] feature
+ */
+fun Routing.kodein() = application.kodein()
+
+/**
+ * Getting the global [Kodein] container from the current [Route]
+ * by browsering all the routing tree until we get to the root level, the [Routing] feature
+ *
+ * @throws IllegalStateException if there is no [Kodein] container
+ */
+fun Route.kodein(): Kodein {
+    val kodein = when (this) {
+        is Routing -> kodein()
+        else -> parent?.kodein()
+    }
+
+    return kodein ?: throw IllegalStateException("No kodein container found for [$this]")
+}
