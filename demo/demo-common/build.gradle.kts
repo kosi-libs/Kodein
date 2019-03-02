6498009b504ba786c26467f2b0fa7a5ce417294b
Romain Bsl <romain.boisselle@protonmail.com>
2019-03-02 19:53:55 +0100

    add the JS target to the demo-common gradle settings

--- demo/demo-common/build.gradle.kts
+++ demo/demo-common/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 kotlin {
     targets.apply {
         add(presets["jvm"].createTarget("jvm"))
+        add(presets["js"].createTarget("js"))
         add(presets["linuxX64"].createTarget("linux").apply { (compilations["main"] as KotlinNativeCompilation).outputKind(NativeOutputKind.EXECUTABLE) })
         add(presets["macosX64"].createTarget("macos").apply { (compilations["main"] as KotlinNativeCompilation).outputKind(NativeOutputKind.EXECUTABLE) })
         add(presets["mingwX64"].createTarget("mingw").apply { (compilations["main"] as KotlinNativeCompilation).outputKind(NativeOutputKind.EXECUTABLE) })
