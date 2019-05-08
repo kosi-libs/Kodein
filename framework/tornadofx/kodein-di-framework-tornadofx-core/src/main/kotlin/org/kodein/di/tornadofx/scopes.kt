fffc7dd139fdab8c833d92f0ec47cef9fb40f9e2
Romain Bsl <romain@boisselle.dev>
2019-05-08 13:50:24 +0200

    add scope for javafx#Node

--- framework/tornadofx/kodein-di-framework-tornadofx-core/src/main/kotlin/org/kodein/di/tornadofx/scopes.kt
+++ framework/tornadofx/kodein-di-framework-tornadofx-core/src/main/kotlin/org/kodein/di/tornadofx/scopes.kt
@@ -1,5 +1,6 @@
 package org.kodein.di.tornadofx
 
+import javafx.scene.*
 import org.kodein.di.bindings.*
 import tornadofx.*
 
@@ -8,3 +9,8 @@ import tornadofx.*
  * like [View] / [Fragment] / [ViewModel] / etc.
  */
 object ComponentScope : WeakContextScope<Component>()
+/**
+ * Scope that will leverage scoping instances on [Node]
+ * Simply, every UI component
+ */
+object NodeScope : WeakContextScope<Node>()
