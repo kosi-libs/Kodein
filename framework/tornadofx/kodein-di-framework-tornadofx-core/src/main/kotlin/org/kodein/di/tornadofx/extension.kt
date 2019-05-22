53eae8637eb654b1cb4c0d1bfbaec95a24560496
Romain Bsl <romain@boisselle.dev>
2019-05-22 09:58:05 +0200

    fix tfx extension to work with binding context instead of KodeinKey#argType

--- framework/tornadofx/kodein-di-framework-tornadofx-core/src/main/kotlin/org/kodein/di/tornadofx/extension.kt
+++ framework/tornadofx/kodein-di-framework-tornadofx-core/src/main/kotlin/org/kodein/di/tornadofx/extension.kt
@@ -10,24 +10,21 @@ import kotlin.reflect.*
 fun Kodein.MainBuilder.installTornadoSource() {
     externalSources += ExternalSource { key ->
         val elementType = key.type.getRaw()?.jvmType as Class<*>?
-        val contextType = key.argType.getRaw()?.jvmType as Class<*>?
 
         // Check if the elementType is injectable by TornadoFX di container (infine if its a Component)
         if (elementType != null &&
                 Component::class.java.isAssignableFrom(elementType)) {
             val componentType = elementType.kotlin as KClass<Component>
+            val scope = this.context
 
-            when (contextType) {
-                // If there is no context argument, we try to find the right Component
-                Unit::class.java -> externalFactory { find(componentType) }
-                else ->
-                    // If there is a context argument it must be of type Scope
-                    // then we try to find the right Component for the given Scope
-                    if (contextType != null && Scope::class.java.isAssignableFrom(contextType))
-                        externalFactory { find(type = componentType, scope = it as Scope) }
-                    else
-                        externalFactory { find(componentType) } // ignore context
-            }
+            // If there is a context it must be of type Scope
+            // then we try to find the right Component for the given Scope
+            if (scope != null &&
+                    Scope::class.java.isAssignableFrom(scope::class.java))
+                externalFactory { find(type = componentType, scope = scope as Scope) }
+            else
+                externalFactory { find(componentType) } // The context is null or not of type Scope
+
         } else null // its not a Component
     }
 }
