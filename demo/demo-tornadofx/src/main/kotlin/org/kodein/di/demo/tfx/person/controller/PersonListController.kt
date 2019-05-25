4e2741c7bed0e9a037754ff89f2747f90ceea96c
Romain Bsl <romain@boisselle.dev>
2019-05-25 10:27:28 +0200

    update example with KodeinAware controller

--- demo/demo-tornadofx/src/main/kotlin/org/kodein/di/demo/tfx/person/controller/PersonListController.kt
+++ demo/demo-tornadofx/src/main/kotlin/org/kodein/di/demo/tfx/person/controller/PersonListController.kt
@@ -1,13 +1,16 @@
 package org.kodein.di.demo.tfx.person.controller
 
 import javafx.collections.*
+import org.kodein.di.*
 import org.kodein.di.demo.tfx.person.model.*
 import org.kodein.di.generic.*
 import org.kodein.di.tornadofx.*
 import tornadofx.*
 
-class PersonListController : Controller() {
-    val personEditorController: PersonEditorController by kodein().instance()
+class PersonListController : Controller(), KodeinAware {
+    override val kodein: Kodein = kodein()
+
+    val personEditorController: PersonEditorController by instance()
 
     lateinit var personList: ObservableList<Person>
 
