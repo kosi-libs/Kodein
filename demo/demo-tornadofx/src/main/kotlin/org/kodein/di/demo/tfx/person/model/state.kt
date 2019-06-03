35f35f457ddba184fe2bafcc05e341f1dcf64aac
Romain Bsl <romain@boisselle.dev>
2019-06-03 15:24:40 +0200

    remove demo-tornadofx project

--- demo/demo-tornadofx/src/main/kotlin/org/kodein/di/demo/tfx/person/model/state.kt
+++
@@ -1,9 +0,0 @@
-package org.kodein.di.demo.tfx.person.model
-
-import javafx.beans.property.*
-import tornadofx.*
-
-class EditingState {
-    val editingProperty = SimpleBooleanProperty(false)
-    var editing by editingProperty
-}
