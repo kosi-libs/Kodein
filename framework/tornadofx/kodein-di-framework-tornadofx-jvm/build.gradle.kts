df7be74623797ad1023d03adc27cd55fcfa90eaa
Romain Bsl <romain@boisselle.dev>
2019-06-04 08:32:38 +0200

    add tests

--- framework/tornadofx/kodein-di-framework-tornadofx-jvm/build.gradle.kts
+++ framework/tornadofx/kodein-di-framework-tornadofx-jvm/build.gradle.kts
@@ -1,4 +1,4 @@
-val tornadofxVersion = "1.7.18"
+import org.jetbrains.kotlin.gradle.tasks.*
 
 plugins {
     id("org.kodein.library.jvm")
@@ -16,6 +16,17 @@ javafx {
     modules("javafx.controls")
 }
 
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
 dependencies {
-    implementation("no.tornado:tornadofx:$tornadofxVersion")
+    implementation("no.tornado:tornadofx:1.7.19")
+    testImplementation(project(":kodein-di-generic-jvm"))
+    testImplementation("org.testfx:testfx-core:4.0.4-alpha")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
 }
