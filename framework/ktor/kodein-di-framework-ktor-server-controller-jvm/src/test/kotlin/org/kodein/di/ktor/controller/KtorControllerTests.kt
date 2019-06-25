75b362e05c37776975a10f40f37966f5d1074a11
Romain Bsl <romain@boisselle.dev>
2019-06-25 10:39:48 +0200

    add `Route` extfun to automatically install route inside the ktor routing feature + test [fix #206]

--- framework/ktor/kodein-di-framework-ktor-server-controller-jvm/src/test/kotlin/org/kodein/di/ktor/controller/KtorControllerTests.kt
+++ framework/ktor/kodein-di-framework-ktor-server-controller-jvm/src/test/kotlin/org/kodein/di/ktor/controller/KtorControllerTests.kt
@@ -3,9 +3,9 @@ package org.kodein.di.ktor.controller
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import org.junit.FixMethodOrder
+import org.junit.*
 import org.junit.Test
-import org.junit.runners.MethodSorters
+import org.junit.runners.*
 import kotlin.test.*
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -41,6 +41,46 @@ class KtorControllerTests {
     fun test_03_failureKodeinControllerImplFeature() {
         assertFailsWith(MissingApplicationFeatureException::class) {
             withTestApplication(Application::kodeincontrollerImplFailure) {}
+        }
+    }
+
+    @Test
+    fun test_00_successKodeinControllerAutoConfiguration(): Unit = withTestApplication(Application::kodeinControllerSuccess) {
+
+        val ROUTE_PROTECTED = "/protected"
+        val ROUTE_FIRST = "/first"
+        val ROUTE_SECOND = "$ROUTE_FIRST/second"
+        val ROUTE_THIRD = "$ROUTE_SECOND/third"
+
+        handleRequest(HttpMethod.Get, ROUTE_VERSION).apply {
+            assertEquals(VERSION, response.content)
+        }
+        handleRequest(HttpMethod.Get, ROUTE_AUTHOR).apply {
+            assertEquals(AUTHOR, response.content)
+        }
+        handleRequest(HttpMethod.Get, "$ROUTE_FIRST$ROUTE_VERSION").apply {
+            assertEquals(VERSION, response.content)
+        }
+        handleRequest(HttpMethod.Get, "$ROUTE_FIRST$ROUTE_AUTHOR").apply {
+            assertEquals(AUTHOR, response.content)
+        }
+        handleRequest(HttpMethod.Get, "$ROUTE_SECOND$ROUTE_VERSION").apply {
+            assertEquals(VERSION, response.content)
+        }
+        handleRequest(HttpMethod.Get, "$ROUTE_SECOND$ROUTE_AUTHOR").apply {
+            assertEquals(AUTHOR, response.content)
+        }
+        handleRequest(HttpMethod.Get, "$ROUTE_THIRD$ROUTE_VERSION").apply {
+            assertEquals(VERSION, response.content)
+        }
+        handleRequest(HttpMethod.Get, "$ROUTE_THIRD$ROUTE_AUTHOR").apply {
+            assertEquals(AUTHOR, response.content)
+        }
+        handleRequest(HttpMethod.Get, "$ROUTE_PROTECTED$ROUTE_VERSION").apply {
+            assertEquals(VERSION, response.content)
+        }
+        handleRequest(HttpMethod.Get, "$ROUTE_PROTECTED$ROUTE_AUTHOR").apply {
+            assertEquals(AUTHOR, response.content)
         }
     }
 }
