65847a4f6fa78e2522ea1331dee6a2c8bb22b13a
Romain Boisselle <romain@boisselle.dev>
2019-07-25 13:58:20 +0200

    source compatibility JVM 1.6

--- demo/demo-android/build.gradle.kts
+++ demo/demo-android/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "kodein.demo"
-        minSdkVersion(21)
+        minSdkVersion(16)
         targetSdkVersion(28)
         versionCode = 1
         versionName = "1.0"
