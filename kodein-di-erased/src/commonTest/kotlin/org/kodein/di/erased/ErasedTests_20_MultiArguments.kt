035ec6a7ceb6b05d48eab9a1566b1143e20c274e
Romain Boisselle <romain@boisselle.dev>
2019-09-01 16:09:17 +0200

    adapt tests for multi arg factories

--- kodein-di-erased/src/commonTest/kotlin/org/kodein/di/erased/ErasedTests_20_MultiArguments.kt
+++ kodein-di-erased/src/commonTest/kotlin/org/kodein/di/erased/ErasedTests_20_MultiArguments.kt
@@ -1,36 +1,39 @@
 package org.kodein.di.erased
 
-import org.kodein.di.Kodein
-import org.kodein.di.direct
+import org.kodein.di.*
 import org.kodein.di.test.*
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
-import kotlin.test.assertNull
+import kotlin.test.*
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class ErasedTests_20_MultiArguments {
 
+    private data class Person(val firstName: String, val lastName: String)
+    private data class MultiArgElement(val a1: String = "", val a2: String = "", val a3: String = "", val a4: String = "", val a5: String = "") {
+        override fun toString(): String {
+            fun concat(prefix: String = "", value: String): String = if (value.isNotEmpty()) "$prefix $value" else ""
+            return "Mr $a1" +
+                    concat(value = a2) +
+                    concat(" of", a3) +
+                    concat(",", a4) +
+                    concat(" in", a5)
+        }
+    }
+
     @Test
     fun test_00_MultiArgumentsFactory() {
         val kodein = Kodein {
-            bind<FullName>() with factory { firstName: String, lastName: String -> FullName(firstName, lastName) }
+            bind<FullName>() with factory { p: Person -> FullName(p.firstName, p.lastName) }
         }
 
-        val i: FullName by kodein.instance(arg = M("Salomon", "BRYS"))
-        val ni: FullName? by kodein.instanceOrNull(arg = M("Salomon", 42))
-        val nni: FullName? by kodein.instanceOrNull(arg = M("Salomon", "BRYS"))
-        val di: FullName = kodein.direct.instance(arg = M("Salomon", "BRYS"))
-        val dni: FullName? = kodein.direct.instanceOrNull(arg = M("Salomon", 42))
-        val dnni: FullName? = kodein.direct.instanceOrNull(arg = M("Salomon", "BRYS"))
-        val p: () -> FullName by kodein.provider(arg = M("Salomon", "BRYS"))
-        val np: (() -> FullName)? by kodein.providerOrNull(arg = M("Salomon", 42))
-        val nnp: (() -> FullName)? by kodein.providerOrNull(arg = M("Salomon", "BRYS"))
-        val dp: () -> FullName = kodein.direct.provider(arg = M("Salomon", "BRYS"))
-        val dnp: (() -> FullName)? = kodein.direct.providerOrNull(arg = M("Salomon", 42))
-        val dnnp: (() -> FullName)? = kodein.direct.providerOrNull(arg = M("Salomon", "BRYS"))
+        val i: FullName by kodein.instance(arg = Person(firstName = "Salomon", lastName = "BRYS"))
+        val nni: FullName? by kodein.instanceOrNull(arg = Person(firstName = "Salomon", lastName = "BRYS"))
+        val di: FullName = kodein.direct.instance(arg = Person(firstName = "Salomon", lastName = "BRYS"))
+        val dnni: FullName? = kodein.direct.instanceOrNull(arg = Person(firstName = "Salomon", lastName = "BRYS"))
+        val p: () -> FullName by kodein.provider(arg = Person(firstName = "Salomon", lastName = "BRYS"))
+        val nnp: (() -> FullName)? by kodein.providerOrNull(arg = Person(firstName = "Salomon", lastName = "BRYS"))
+        val dp: () -> FullName = kodein.direct.provider(arg = Person(firstName = "Salomon", lastName = "BRYS"))
+        val dnnp: (() -> FullName)? = kodein.direct.providerOrNull(arg = Person(firstName = "Salomon", lastName = "BRYS"))
 
-        assertAllNull(ni, dni, np, dnp)
         assertAllNotNull(nni, dnni, nnp, dnnp)
 
         assertAllEqual(FullName("Salomon", "BRYS"), i, nni!!, di, dnni, p(), nnp!!(), dp(), dnnp!!())
@@ -76,84 +79,48 @@ class ErasedTests_20_MultiArguments {
     @Test
     fun test_03_BigMultiArgumentsFactories() {
         val kodein = Kodein {
-            bind<String>() with factory { a: String -> "Mr $a" }
-            bind<String>() with factory { a: String, b: String -> "Mr $a $b" }
-            bind<String>() with factory { a: String, b: String, c: String -> "Mr $a $b of $c" }
-            bind<String>() with factory { a: String, b: String, c: String, d: String -> "Mr $a $b of $c, $d" }
-            bind<String>() with factory { a: String, b: String, c: String, d: String, e: String -> "Mr $a $b of $c, $d in $e" }
+            bind<String>() with factory { m: MultiArgElement -> m.toString() }
         }
 
-        assertEquals("Mr Salomon", kodein.direct.instance(arg = "Salomon"))
-        assertEquals("Mr Salomon BRYS", kodein.direct.instance(arg = M("Salomon", "BRYS")))
-        assertEquals("Mr Salomon BRYS of Paris", kodein.direct.instance(arg = M("Salomon", "BRYS", "Paris")))
-        assertEquals("Mr Salomon BRYS of Paris, France", kodein.direct.instance(arg = M("Salomon", "BRYS", "Paris", "France")))
-        assertEquals("Mr Salomon BRYS of Paris, France in Europe", kodein.direct.instance(arg = M("Salomon", "BRYS", "Paris", "France", "Europe")))
+        assertEquals("Mr Salomon", kodein.direct.instance(arg = MultiArgElement("Salomon")))
+        assertEquals("Mr Salomon BRYS", kodein.direct.instance(arg = MultiArgElement("Salomon", "BRYS")))
+        assertEquals("Mr Salomon BRYS of Paris", kodein.direct.instance(arg = MultiArgElement("Salomon", "BRYS", "Paris")))
+        assertEquals("Mr Salomon BRYS of Paris, France", kodein.direct.instance(arg = MultiArgElement("Salomon", "BRYS", "Paris", "France")))
+        assertEquals("Mr Salomon BRYS of Paris, France in Europe", kodein.direct.instance(arg = MultiArgElement("Salomon", "BRYS", "Paris", "France", "Europe")))
     }
 
     @Test
     fun test_04_MultiArgumentsFactoryFunction() {
         val kodein = Kodein {
-            bind<String>() with factory { a: String -> "Mr $a" }
-            bind<String>() with factory { a: String, b: String -> "Mr $a $b" }
-            bind<String>() with factory { a: String, b: String, c: String -> "Mr $a $b of $c" }
-            bind<String>() with factory { a: String, b: String, c: String, d: String -> "Mr $a $b of $c, $d" }
-            bind<String>() with factory { a: String, b: String, c: String, d: String, e: String -> "Mr $a $b of $c, $d in $e" }
+            bind<String>() with factory { m: MultiArgElement -> m.toString() }
         }
 
-        val f1: (String) -> String by kodein.factory()
-        val f2: (String, String) -> String by kodein.factory2()
-        val f3: (String, String, String) -> String by kodein.factory3()
-        val f4: (String, String, String, String) -> String by kodein.factory4()
-        val f5: (String, String, String, String, String) -> String by kodein.factory5()
-        val fn1: ((Int) -> String)? by kodein.factoryOrNull()
-        val fn2: ((Int, Int) -> String)? by kodein.factory2OrNull()
-        val fn3: ((Int, Int, Int) -> String)? by kodein.factory3OrNull()
-        val fn4: ((Int, Int, Int, Int) -> String)? by kodein.factory4OrNull()
-        val fn5: ((Int, Int, Int, Int, Int) -> String)? by kodein.factory5OrNull()
+        val factory: (MultiArgElement) -> String by kodein.factory()
+        val factoryNull: ((Int) -> String)? by kodein.factoryOrNull()
 
-        assertEquals("Mr Salomon", f1("Salomon"))
-        assertEquals("Mr Salomon BRYS", f2("Salomon", "BRYS"))
-        assertEquals("Mr Salomon BRYS of Paris", f3("Salomon", "BRYS", "Paris"))
-        assertEquals("Mr Salomon BRYS of Paris, France", f4("Salomon", "BRYS", "Paris", "France"))
-        assertEquals("Mr Salomon BRYS of Paris, France in Europe", f5("Salomon", "BRYS", "Paris", "France", "Europe"))
-        assertNull(fn1)
-        assertNull(fn2)
-        assertNull(fn3)
-        assertNull(fn4)
-        assertNull(fn5)
+        assertEquals("Mr Salomon", factory(MultiArgElement("Salomon")))
+        assertEquals("Mr Salomon BRYS", factory(MultiArgElement("Salomon", "BRYS")))
+        assertEquals("Mr Salomon BRYS of Paris", factory(MultiArgElement("Salomon", "BRYS", "Paris")))
+        assertEquals("Mr Salomon BRYS of Paris, France", factory(MultiArgElement("Salomon", "BRYS", "Paris", "France")))
+        assertEquals("Mr Salomon BRYS of Paris, France in Europe", factory(MultiArgElement("Salomon", "BRYS", "Paris", "France", "Europe")))
+        assertNull(factoryNull)
     }
 
     @Test
     fun test_05_MultiArgumentsFactoryDirectFunction() {
         val kodein = Kodein.direct {
-            bind<String>() with factory { a: String -> "Mr $a" }
-            bind<String>() with factory { a: String, b: String -> "Mr $a $b" }
-            bind<String>() with factory { a: String, b: String, c: String -> "Mr $a $b of $c" }
-            bind<String>() with factory { a: String, b: String, c: String, d: String -> "Mr $a $b of $c, $d" }
-            bind<String>() with factory { a: String, b: String, c: String, d: String, e: String -> "Mr $a $b of $c, $d in $e" }
+            bind<String>() with factory { m: MultiArgElement -> m.toString() }
         }
 
-        val f1: (String) -> String = kodein.factory()
-        val f2: (String, String) -> String = kodein.factory2()
-        val f3: (String, String, String) -> String = kodein.factory3()
-        val f4: (String, String, String, String) -> String = kodein.factory4()
-        val f5: (String, String, String, String, String) -> String = kodein.factory5()
-        val fn1: ((Int) -> String)? = kodein.factoryOrNull()
-        val fn2: ((Int, Int) -> String)? = kodein.factory2OrNull()
-        val fn3: ((Int, Int, Int) -> String)? = kodein.factory3OrNull()
-        val fn4: ((Int, Int, Int, Int) -> String)? = kodein.factory4OrNull()
-        val fn5: ((Int, Int, Int, Int, Int) -> String)? = kodein.factory5OrNull()
+        val factory: (MultiArgElement) -> String = kodein.factory()
+        val factoryNull: ((Int) -> String)? = kodein.factoryOrNull()
 
-        assertEquals("Mr Salomon", f1("Salomon"))
-        assertEquals("Mr Salomon BRYS", f2("Salomon", "BRYS"))
-        assertEquals("Mr Salomon BRYS of Paris", f3("Salomon", "BRYS", "Paris"))
-        assertEquals("Mr Salomon BRYS of Paris, France", f4("Salomon", "BRYS", "Paris", "France"))
-        assertEquals("Mr Salomon BRYS of Paris, France in Europe", f5("Salomon", "BRYS", "Paris", "France", "Europe"))
-        assertNull(fn1)
-        assertNull(fn2)
-        assertNull(fn3)
-        assertNull(fn4)
-        assertNull(fn5)
+        assertEquals("Mr Salomon", factory(MultiArgElement("Salomon")))
+        assertEquals("Mr Salomon BRYS", factory(MultiArgElement("Salomon", "BRYS")))
+        assertEquals("Mr Salomon BRYS of Paris", factory(MultiArgElement("Salomon", "BRYS", "Paris")))
+        assertEquals("Mr Salomon BRYS of Paris, France", factory(MultiArgElement("Salomon", "BRYS", "Paris", "France")))
+        assertEquals("Mr Salomon BRYS of Paris, France in Europe", factory(MultiArgElement("Salomon", "BRYS", "Paris", "France", "Europe")))
+        assertNull(factoryNull)
     }
     
 }
