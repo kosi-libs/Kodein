82e6078182f7b5432d60759f733d54140b493671
Romain Boisselle <romain@boisselle.dev>
2020-05-28 08:56:02 +0200

    pepare for next version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         maven(url = "https://dl.bintray.com/kodein-framework/Kodein-Internal-Gradle")
     }
     dependencies {
-        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:3.4.4")
+        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:3.4.5")
     }
 }
 
