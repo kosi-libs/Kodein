a6ac435d62a8687a0caf79c595b3f30a783f5683
Romain Boisselle <romain@boisselle.dev>
2020-07-25 13:57:33 +0200

    Update CI

--- kodein-di/build.gradle.kts
+++ kodein-di/build.gradle.kts
@@ -7,7 +7,7 @@ kodein {
 
         common {
             main.dependencies {
-                api("org.kodein.type:kodein-type:1.2.0-kotlin-1.4-M3-78")
+                api("org.kodein.type:kodein-type:1.2.0-kotlin-1.4-M3-90")
             }
             test.dependencies {
                 implementation(project(":test-utils"))
