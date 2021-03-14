6f9d0205ebe0c854ec9ddb1d2b354a62226ebfa8
Romain Boisselle <romain@boisselle.dev>
2021-03-14 20:41:32 +0100

    fix jxinjector module compilation

--- kodein-di-jxinject-jvm/src/test/kotlin/org/kodein/di/jxinject/kodeins.kt
+++ kodein-di-jxinject-jvm/src/test/kotlin/org/kodein/di/jxinject/kodeins.kt
@@ -5,8 +5,8 @@ import org.kodein.di.*
 fun test0() = DI {
     import(jxInjectorModule)
 
-    bind() from instance("Salomon")
-    bind(tag = "lastname") from instance("BRYS")
+    bind { instance("Salomon") }
+    bind(tag = "lastname") { instance("BRYS") }
     bind<List<String>>() with instance(listOf("a", "b", "c"))
     bind<Set<String>>() with instance(setOf("a", "b", "c"))
 }
@@ -16,8 +16,8 @@ fun test1() = DI.direct {
 
     var count = 0
 
-    bind() from provider { "Salomon ${count++}" }
-    bind(tag = "lastname") from provider { "BRYS ${count++}" }
+    bind { provider { "Salomon ${count++}" } }
+    bind(tag = "lastname") { provider { "BRYS ${count++}" } }
     bind<List<String>>() with provider { listOf("a", "b", "c") }
     bind<Set<String>>() with provider { setOf("a", "b", "c") }
 }
@@ -25,14 +25,14 @@ fun test1() = DI.direct {
 fun test2() = DI {
     import(jxInjectorModule)
 
-    bind() from factory { i: Int -> "Salomon $i" }
-    bind(tag = "lastname") from factory { i: Int -> "BRYS $i" }
+    bind { factory { i: Int -> "Salomon $i" } }
+    bind(tag = "lastname") { factory { i: Int -> "BRYS $i" } }
     bind<List<String>>() with factory { i: Int -> listOf("a$i", "b$i", "c$i") }
     bind<Set<String>>() with factory { i: Int -> setOf("a$i", "b$i", "c$i") }
 }
 
 fun test4() = DI.direct {
-    bind(tag = "universe:answer") from instance("fourty-two")
+    bind(tag = "universe:answer") { instance("fourty-two") }
 
     import(jxInjectorModule)
 
