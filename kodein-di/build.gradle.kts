91488ad28903fd279349bb74bf71593558c62fc5
Romain Boisselle <romain@boisselle.dev>
2021-05-21 05:55:33 +0200

    docs + upgrade Kodein-Type

--- kodein-di/build.gradle.kts
+++ kodein-di/build.gradle.kts
@@ -7,7 +7,7 @@ kodein {
 
         common {
             main.dependencies {
-                api("org.kodein.type:kodein-type:1.7.0")
+                api("org.kodein.type:kodein-type:1.7.1")
             }
             test.dependencies {
                 implementation(project(":test-utils"))
