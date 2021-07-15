435d8647937cf0c451acb2f4fade2459a68d5d74
Romain Boisselle <romain@boisselle.dev>
2021-07-15 11:06:48 +0200

    upgrade internal gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         maven(url = "https://raw.githubusercontent.com/Kodein-Framework/kodein-internal-gradle-plugin/mvn-repo")
     }
     dependencies {
-        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:6.5.1")
+        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:6.6.0")
     }
 }
 
