c0b3c7d4ecdd0e9fbba4a38590e1ba5ef676cf29
Romain Boisselle <romain@boisselle.dev>
2021-07-25 07:43:45 +0200

    add bindEagerSingleton shortcut (fixes #379)

--- kodein-di/src/commonMain/kotlin/org/kodein/di/DIBuilder.kt
+++ kodein-di/src/commonMain/kotlin/org/kodein/di/DIBuilder.kt
@@ -16,6 +16,16 @@ import org.kodein.type.generic
 public inline fun <reified T: Any> DI.Builder.bindSingleton(tag: Any? = null, overrides: Boolean? = null, sync: Boolean = true, noinline creator: DirectDI.() -> T): Unit = Bind(tag = tag, overrides = overrides, binding = singleton(sync = sync, creator = creator))
 
 /**
+ * Binds an eager singleton: will create an instance as soon as kodein is ready (all bindings are set) and will always return this instance.
+ *
+ * T generics will be erased!
+ *
+ * @param T The created type.
+ * @param creator The function that will be called as soon as DI is ready. Guaranteed to be called only once. Should create a new instance.
+ */
+public inline fun <reified T: Any> DI.Builder.bindEagerSingleton(tag: Any? = null, overrides: Boolean? = null, noinline creator: DirectDI.() -> T): Unit = Bind(tag = tag, overrides = overrides, binding = eagerSingleton(creator = creator))
+
+/**
  * Binds a multiton: will create an instance on first request and will subsequently always return the same instance.
  *
  * T generics will be erased!
