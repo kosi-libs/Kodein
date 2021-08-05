afb34094e1530b9f5d3da0e4e62bb24dd209ef09
Carlton Whitehead <carlton.whitehead@genesys.com>
2021-08-05 11:29:20 -0400

    Update dependencies, remove unnecessary ones

--- framework/android/kodein-di-framework-android-x-viewmodel-savedstate/build.gradle.kts
+++ framework/android/kodein-di-framework-android-x-viewmodel-savedstate/build.gradle.kts
@@ -4,17 +4,14 @@ plugins {
 
 dependencies {
     api(project(":framework:android:kodein-di-framework-android-x"))
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("androidx.activity:activity-ktx:1.2.3")
-    implementation("androidx.fragment:fragment-ktx:1.3.5")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
     val lifecycleVersion = "2.3.0"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
 }
 
 kodeinUpload {
     name = "Kodein-DI-Framework-AndroidX-ViewModel-SavedState"
-    description = "Kodein-DI extensions AndroidX ViewModel with SavedStateHandle"
+    description = "Kodein-DI extensions for AndroidX ViewModel with SavedStateHandle"
 }
