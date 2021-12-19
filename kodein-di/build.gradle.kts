a8cc77d06942e80702972caf3006036561d4ad91
Romain Boisselle <romain@boisselle.dev>
2021-12-19 08:32:39 +0100

    kotlin 1.6.10 + Compose 1.0.1-rc2

--- kodein-di/build.gradle.kts
+++ kodein-di/build.gradle.kts
@@ -7,7 +7,7 @@ kodein {
 
         common {
             main.dependencies {
-                api("org.kodein.type:kodein-type:1.11.0")
+                api("org.kodein.type:kodein-type:1.12.0")
             }
             test.dependencies {
                 implementation(project(":test-utils"))
