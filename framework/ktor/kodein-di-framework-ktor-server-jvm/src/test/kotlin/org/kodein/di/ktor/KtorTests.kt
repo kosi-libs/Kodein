7d846918ebe10c3d73197776d2752c25e8770958
Romain Boisselle <romain@boisselle.dev>
2021-12-25 17:50:25 +0100

    fix ktor 2.0 tests

--- framework/ktor/kodein-di-framework-ktor-server-jvm/src/test/kotlin/org/kodein/di/ktor/KtorTests.kt
+++ framework/ktor/kodein-di-framework-ktor-server-jvm/src/test/kotlin/org/kodein/di/ktor/KtorTests.kt
@@ -1,10 +1,17 @@
 package org.kodein.di.ktor
 
+import io.ktor.client.call.*
+import io.ktor.client.plugins.cookies.*
+import io.ktor.client.request.*
 import io.ktor.server.application.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import io.ktor.util.*
+import kotlinx.coroutines.coroutineScope
 import org.junit.FixMethodOrder
 import org.junit.runners.MethodSorters
+import org.kodein.di.DI
+import org.kodein.di.LazyDI
 import kotlin.test.*
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -27,178 +34,145 @@ class KtorTests {
 
 
     @Test
-    fun test_00_getSession(): Unit = withTestApplication(Application::main) {
-        handleRequest(HttpMethod.Get, ROUTE_SESSION).apply {
-            val content = response.content
-            assertNotNull(content)
-            assertContained("java.util.Random@.*".toRegex(), content)
-        }
+    fun test_00_getSession(): Unit = testApplication {
+        application(Application::main)
+
+        val body = client.get(ROUTE_SESSION).body<String?>()
+        assertNotNull(body)
+        assertContained("java.util.Random@.*".toRegex(), body)
     }
 
     @Test
-    fun test_01_getIncrement(): Unit = withTestApplication(Application::main) {
-        handleRequest(HttpMethod.Get, ROUTE_SESSION + ROUTE_INC).apply {
-            assertEquals(MockSession(1).toString(), response.content)
-        }
+    fun test_01_getIncrement(): Unit = testApplication {
+        application(Application::main)
+        assertEquals(MockSession(1).toString(), client.get(ROUTE_SESSION + ROUTE_INC).body())
     }
 
     @Test
-    fun test_02_getClear(): Unit = withTestApplication(Application::main) {
-        handleRequest(HttpMethod.Get, ROUTE_SESSION + ROUTE_CLEAR).apply {
-            assertEquals("null", response.content)
-        }
+    fun test_02_getClear(): Unit = testApplication {
+        application(Application::main)
+        assertEquals("null", client.get(ROUTE_SESSION + ROUTE_CLEAR).body())
     }
 
     @Test
-    fun test_03_sessionScoped(): Unit = withTestApplication(Application::main) {
-        cookiesSession {
-            // set a session context
-            val sessionRandomPairs = mutableListOf<Pair<MockSession, String>>()
-            val sessionCookie: Cookie
+    fun test_03_sessionScoped(): Unit = testApplication {
+        application(Application::main)
 
-            // (1) Call '/session' for the first time
-            // Should send a new instance of SecureRandom
-            // Session is actually MockSession(counter=0) - default value
-            handleRequest(HttpMethod.Get, ROUTE_SESSION).apply {
-                val newRandomInstance = response.content.toString()
-                sessionRandomPairs.add(MockSession() to newRandomInstance)
-            }
+        // set a session context
+        val sessionRandomPairs = mutableListOf<Pair<MockSession, String>>()
+        val sessionCookie: Cookie
 
-            // (2) Call '/session/increment' to create a session - MockSession(counter=1)
-            handleRequest(HttpMethod.Get, ROUTE_SESSION + ROUTE_INC).apply {
-                sessionCookie = response.headers.values(HttpHeaders.SetCookie)
-                        .map { parseServerSetCookieHeader(it) }
-                        .first { it.name == SESSION_FEATURE_SESSION_ID }
+        // (1) Call '/session' for the first time
+        // Should send a new instance of SecureRandom
+        // Session is actually MockSession(counter=0) - default value
+        sessionRandomPairs.add(MockSession() to client.get(ROUTE_SESSION).body())
 
-                assertNotNull(sessionCookie)
-                assertNotEquals(NO_SESSION, sessionCookie.value)
-            }
-
-            // (3) Call '/session' with the session - MockSession(counter=1)
-            // Should send a new instance of SecureRandom != from (1)
-            handleRequest(HttpMethod.Get, ROUTE_SESSION) {
-                addHeader(HttpHeaders.Cookie, sessionCookie.toString())
-            }.apply {
-                val newRandomInstance = response.content ?: "null"
-                val pairs = sessionRandomPairs.toList()
-                sessionRandomPairs.add(MockSession(1) to newRandomInstance)
-                assertNone(pairs) { it.second == newRandomInstance }
-            }
-
-            // (4) Call '/session/increment' to create a new session - MockSession(counter=2)
-            // Session cookie is still the same
-            handleRequest(HttpMethod.Get, ROUTE_SESSION + ROUTE_INC) {
-                addHeader(HttpHeaders.Cookie, sessionCookie.toString())
-            }.apply {
-                val cookie = response.headers.values(HttpHeaders.SetCookie)
-                        .map { parseServerSetCookieHeader(it) }
-                        .first { it.name == SESSION_FEATURE_SESSION_ID }
-
-                assertNotNull(cookie)
-                assertEquals(sessionCookie.name, cookie.name)
-                assertEquals(sessionCookie.value, cookie.value)
-            }
-
-            // (5) Call '/session' with the session MockSession(counter=2)
-            // Should send a new instance of SecureRandom != from (1) and (3)
-            handleRequest(HttpMethod.Get, ROUTE_SESSION) {
-                addHeader(HttpHeaders.Cookie, sessionCookie.toString())
-            }.apply {
-                val newRandomInstance = response.content ?: "null"
-                val pairs = sessionRandomPairs.toList()
-                sessionRandomPairs.add(MockSession(2) to newRandomInstance)
-
-                assertNone(pairs) { it.second == newRandomInstance }
-            }
-
-            // (6) Call '/session/clear'
-            // Should return 'null
-            handleRequest(HttpMethod.Get, ROUTE_SESSION + ROUTE_CLEAR) {
-                addHeader(HttpHeaders.Cookie, sessionCookie.toString())
-            }.apply {
-                assertEquals("null", response.content)
-            }
-
-            // (7) Call '/session' after clearing the session
-            // Should send a scoped instance of SecureRandom; same as (1)
-            // Session is actually MockSession(counter=0) - default value
-            handleRequest(HttpMethod.Get, ROUTE_SESSION).apply {
-                assertAny(sessionRandomPairs) { it.first == MockSession() && it.second == response.content }
-            }
+        // (2) Call '/session/increment' to create a session - MockSession(counter=1)
+        client.get(ROUTE_SESSION + ROUTE_INC).apply {
+            sessionCookie = headers[HttpHeaders.SetCookie]
+                ?.let { parseServerSetCookieHeader(it) }  ?: fail()
+            assertNotNull(sessionCookie)
+            assertNotEquals(NO_SESSION, sessionCookie.value)
         }
-    }
 
-
-    @Test
-    fun test_04_getRequestScoped(): Unit = withTestApplication(Application::main) {
-        handleRequest(HttpMethod.Get, ROUTE_REQUEST).apply {
-            assertNotNull(response.content)
-
-            response.content?.let {
-                val pairs = it.split(",")
-                        .map {
-                            val keyValue = it.split("=")
-                            keyValue.first() to keyValue.last()
-                        }
-
-                assertEquals(5, pairs.size) // Ensure we pass through 5 phases (Setup, Monitoring, Features, Call, GET)
-
-                assertEquals(1, pairs.map { it.second }.distinct().count()) // For all the phases we only have 1 Random instance
-            }
-        }
-    }
-
-    @Test
-    fun test_05_closest(): Unit = withTestApplication(Application::main) {
-        val diInstance = this.application.attributes[KodeinDIKey]
-
-        assertSame(diInstance, closestDI { this.application }.baseDI)
-
-        handleRequest(HttpMethod.Get, ROUTE_CLOSEST) {
-            assertSame(diInstance, closestDI { this.call.application }.baseDI)
+        // (3) Call '/session' with the session - MockSession(counter=1)
+        // Should send a new instance of SecureRandom != from (1)
+        client.get(ROUTE_SESSION) {
+            cookie(sessionCookie.name, sessionCookie.value)
         }.apply {
-            assertNotNull(response.content)
+            val newRandomInstance = body() ?: "null"
+            val pairs = sessionRandomPairs.toList()
+            sessionRandomPairs.add(MockSession(1) to newRandomInstance)
+            assertNone(pairs) { it.second == newRandomInstance }
+        }
 
-            response.content?.let {
-                val diInstances = it.split(",").map { it.trim() }
-                assertEquals(1, diInstances.distinct().size)
-                assertEquals("$diInstance", diInstances.first())
-            }
 
-            assertSame(diInstance, closestDI().baseDI)
+        // (4) Call '/session/increment' to create a new session - MockSession(counter=2)
+        // Session cookie is still the same
+        client.get(ROUTE_SESSION + ROUTE_INC) {
+            cookie(sessionCookie.name, sessionCookie.value)
+        }.apply {
+            val cookie = headers[HttpHeaders.SetCookie]
+                ?.let { parseServerSetCookieHeader(it) } ?: fail()
+
+            assertNotNull(cookie)
+            assertEquals(sessionCookie.name, cookie.name)
+            assertEquals(sessionCookie.value, cookie.value)
+        }
+
+        // (5) Call '/session' with the session MockSession(counter=2)
+        // Should send a new instance of SecureRandom != from (1) and (3)
+        client.get(ROUTE_SESSION) {
+            cookie(sessionCookie.name, sessionCookie.value)
+        }.apply {
+            val newRandomInstance = body() ?: "null"
+            val pairs = sessionRandomPairs.toList()
+            sessionRandomPairs.add(MockSession(2) to newRandomInstance)
+
+            assertNone(pairs) { it.second == newRandomInstance }
+        }
+
+        // (6) Call '/session/clear'
+        // Should return 'null
+        client.get(ROUTE_SESSION + ROUTE_CLEAR) {
+            cookie(sessionCookie.name, sessionCookie.value)
+        }.apply {
+            assertEquals("null", body())
+        }
+
+        // (7) Call '/session' after clearing the session
+        // Should send a scoped instance of SecureRandom; same as (1)
+        // Session is actually MockSession(counter=0) - default value
+        client.get(ROUTE_SESSION).apply {
+            val content = body<String>()
+            assertAny(sessionRandomPairs) { it.first == MockSession() && it.second == content }
         }
     }
 
     @Test
-    fun test_06_subKodein(): Unit = withTestApplication(Application::main) {
-        val diInstance = this.application.attributes[KodeinDIKey]
+    fun test_04_getRequestScoped(): Unit = testApplication {
+        application(Application::main)
 
-        assertSame(diInstance, closestDI { this.application }.baseDI)
+        val pairs = client.get(ROUTE_REQUEST).body<String>().split(",")
+                .map {
+                    val keyValue = it.split("=")
+                    keyValue.first() to keyValue.last()
+                }
 
-        handleRequest(HttpMethod.Post, "$ROUTE_SUBKODEIN$ROUTE_SUB_UPPER") {
-            assertSame(diInstance, closestDI { this.call.application }.baseDI)
-        }.apply {
-            assertNotNull(response.content)
+        assertEquals(5, pairs.size) // Ensure we pass through 5 phases (Setup, Monitoring, Features, Call, GET)
+        assertEquals(1, pairs.map { it.second }.distinct().count()) // For all the phases we only have 1 Random instance
+    }
 
-            response.content?.let {
-                val diInstances = it.split(",").map { it.trim() }
-                assertEquals(2, diInstances.distinct().size)
-                assertEquals("$diInstance", diInstances.first())
-            }
 
-            assertSame(diInstance, closestDI().baseDI)
+    @Test
+    fun test_05_closest(): Unit = testApplication {
+        application(Application::main)
+
+        client.get(ROUTE_CLOSEST).body<String>().apply {
+            val diInstances = split(",").map { it.trim() }
+            assertEquals(1, diInstances.distinct().size)
         }
+    }
+
+
+    @Test
+    fun test_06_subKodein(): Unit = testApplication {
+        application(Application::main)
+
+        val body = client.post("$ROUTE_SUBKODEIN$ROUTE_SUB_UPPER").body<String>()
+        val diInstances = body.split(",").map { it.trim() }
+        assertEquals(2, diInstances.distinct().size)
 
         val lowerAuthor: String
         val upperAuthor: String
         val NO_RESPONSE = "NO_RESPONSE"
 
-        handleRequest(HttpMethod.Get, "$ROUTE_SUBKODEIN$ROUTE_SUB_LOWER").apply {
-            lowerAuthor = response.content ?: NO_RESPONSE
+        client.get("$ROUTE_SUBKODEIN$ROUTE_SUB_LOWER").apply {
+            lowerAuthor = body() ?: NO_RESPONSE
             assertEquals(AUTHOR.lowercase(), lowerAuthor)
         }
-        handleRequest(HttpMethod.Get, "$ROUTE_SUBKODEIN$ROUTE_SUB_UPPER").apply {
-            upperAuthor = response.content ?: NO_RESPONSE
+        client.get("$ROUTE_SUBKODEIN$ROUTE_SUB_UPPER").apply {
+            upperAuthor = body() ?: NO_RESPONSE
             assertEquals(AUTHOR.uppercase(), upperAuthor)
         }
 
