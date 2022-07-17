e2a1e36ebd0d0e17e8611b308a62a3ca013fb8d0
Romain BOISSELLE <romain.boisselle@proton.me>
2022-07-17 21:08:27 +0200

    Gradle 7.5 + Kotlin 1.7.10 + Kaverit 2.1.0

--- kodein-di/build.gradle.kts
+++ kodein-di/build.gradle.kts
@@ -7,7 +7,7 @@ kodein {
 
         common {
             main.dependencies {
-                api("org.kodein.type:kaverit:2.0.1")
+                api("org.kodein.type:kaverit:2.1.0")
             }
             test.dependencies {
                 implementation(project(":test-utils"))
