b639595bd0c7fe47c7eba75ed09f86e3f7257389
Romain Boisselle <romain.boisselle@proton.me>
2023-03-31 13:42:06 +0200

    add Jetpack Compose ViewModel factory retrieval

--- framework/compose/kodein-di-framework-compose/src/androidMain/kotlin/org/kodein/di/compose/retrievingAndroid.kt
+++ framework/compose/kodein-di-framework-compose/src/androidMain/kotlin/org/kodein/di/compose/retrievingAndroid.kt
@@ -39,3 +39,34 @@ public inline fun <reified VM : ViewModel> rememberViewModel(tag: Any? = null): 
         )
     }
 }
+
+/**
+ * Gets an instance of a [VM] as an android [ViewModel] for the given [tag].
+ *
+ * VM generic will be preserved!
+ *
+ * @param VM The type of the [ViewModel] to retrieve.
+ * @param tag The bound tag, if any.
+ * @return An instance of [VM].
+ * @throws DI.NotFoundException If no provider was found.
+ * @throws DI.DependencyLoopException If the value construction triggered a dependency loop.
+ */
+@Composable
+public inline fun <reified A: Any, reified VM : ViewModel> rememberViewModel(tag: Any? = null, arg: A): ViewModelLazy<VM> = with(localDI()) {
+    val viewModelStoreOwner = LocalViewModelStoreOwner.current ?: error("")
+
+    remember {
+        ViewModelLazy(
+            viewModelClass = VM::class,
+            storeProducer = { viewModelStoreOwner.viewModelStore },
+            factoryProducer = {
+                object : ViewModelProvider.Factory {
+                    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                        @Suppress("UNCHECKED_CAST")
+                        return direct.instance<A, VM>(tag, arg) as T
+                    }
+                }
+            }
+        )
+    }
+}
