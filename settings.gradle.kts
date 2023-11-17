9dcd7bfb6ef0be1062646b4e642d0c10cb032fae
Romain Boisselle <romain.boisselle@proton.me>
2023-11-17 17:11:27 +0100

    fix compose modules compilation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         maven(url = "https://raw.githubusercontent.com/kosi-libs/kodein-internal-gradle-plugin/mvn-repo")
     }
     dependencies {
-        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:8.3.0")
+        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:8.3.1")
     }
 }
 
