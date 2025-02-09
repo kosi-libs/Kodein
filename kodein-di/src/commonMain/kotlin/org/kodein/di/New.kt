8e49b301f0d683f290e1d9b85ad0e3daf66858eb
Nek-12 <vaizin.nikita@gmail.com>
2025-02-09 15:48:04 +0100

    add new up to 22 parameters

--- kodein-di/src/commonMain/kotlin/org/kodein/di/New.kt
+++ kodein-di/src/commonMain/kotlin/org/kodein/di/New.kt
@@ -130,3 +130,414 @@ public inline fun <T,
     instance(), instance(), instance(), instance(), instance(),
     instance(), instance(), instance(), instance(), instance()
 )
+
+
+/**
+ * @see new
+ */
+public inline fun <
+        T,
+        reified P1,
+        reified P2,
+        reified P3,
+        reified P4,
+        reified P5,
+        reified P6,
+        reified P7,
+        reified P8,
+        reified P9,
+        reified P10,
+        reified P11
+        > DirectDIAware.new(
+    constructor: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11) -> T,
+): T = constructor(
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance()
+)
+
+/**
+ * @see new
+ */
+public inline fun <
+        T,
+        reified P1,
+        reified P2,
+        reified P3,
+        reified P4,
+        reified P5,
+        reified P6,
+        reified P7,
+        reified P8,
+        reified P9,
+        reified P10,
+        reified P11,
+        reified P12
+        > DirectDIAware.new(
+    constructor: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12) -> T,
+): T = constructor(
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance()
+)
+
+/**
+ * @see new
+ */
+public inline fun <
+        T,
+        reified P1,
+        reified P2,
+        reified P3,
+        reified P4,
+        reified P5,
+        reified P6,
+        reified P7,
+        reified P8,
+        reified P9,
+        reified P10,
+        reified P11,
+        reified P12,
+        reified P13
+        > DirectDIAware.new(
+    constructor: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13) -> T,
+): T = constructor(
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance()
+)
+
+/**
+ * @see new
+ */
+public inline fun <
+        T,
+        reified P1,
+        reified P2,
+        reified P3,
+        reified P4,
+        reified P5,
+        reified P6,
+        reified P7,
+        reified P8,
+        reified P9,
+        reified P10,
+        reified P11,
+        reified P12,
+        reified P13,
+        reified P14
+        > DirectDIAware.new(
+    constructor: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14) -> T,
+): T = constructor(
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance()
+)
+
+/**
+ * @see new
+ */
+public inline fun <
+        T,
+        reified P1,
+        reified P2,
+        reified P3,
+        reified P4,
+        reified P5,
+        reified P6,
+        reified P7,
+        reified P8,
+        reified P9,
+        reified P10,
+        reified P11,
+        reified P12,
+        reified P13,
+        reified P14,
+        reified P15
+        > DirectDIAware.new(
+    constructor: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15) -> T,
+): T = constructor(
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance()
+)
+
+/**
+ * @see new
+ */
+public inline fun <
+        T,
+        reified P1,
+        reified P2,
+        reified P3,
+        reified P4,
+        reified P5,
+        reified P6,
+        reified P7,
+        reified P8,
+        reified P9,
+        reified P10,
+        reified P11,
+        reified P12,
+        reified P13,
+        reified P14,
+        reified P15,
+        reified P16
+        > DirectDIAware.new(
+    constructor: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16) -> T,
+): T = constructor(
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance()
+)
+
+/**
+ * @see new
+ */
+public inline fun <
+        T,
+        reified P1,
+        reified P2,
+        reified P3,
+        reified P4,
+        reified P5,
+        reified P6,
+        reified P7,
+        reified P8,
+        reified P9,
+        reified P10,
+        reified P11,
+        reified P12,
+        reified P13,
+        reified P14,
+        reified P15,
+        reified P16,
+        reified P17
+        > DirectDIAware.new(
+    constructor: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17) -> T,
+): T = constructor(
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance()
+)
+
+/**
+ * @see new
+ */
+public inline fun <
+        T,
+        reified P1,
+        reified P2,
+        reified P3,
+        reified P4,
+        reified P5,
+        reified P6,
+        reified P7,
+        reified P8,
+        reified P9,
+        reified P10,
+        reified P11,
+        reified P12,
+        reified P13,
+        reified P14,
+        reified P15,
+        reified P16,
+        reified P17,
+        reified P18
+        > DirectDIAware.new(
+    constructor: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18) -> T,
+): T = constructor(
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance()
+)
+
+/**
+ * @see new
+ */
+public inline fun <
+        T,
+        reified P1,
+        reified P2,
+        reified P3,
+        reified P4,
+        reified P5,
+        reified P6,
+        reified P7,
+        reified P8,
+        reified P9,
+        reified P10,
+        reified P11,
+        reified P12,
+        reified P13,
+        reified P14,
+        reified P15,
+        reified P16,
+        reified P17,
+        reified P18,
+        reified P19
+        > DirectDIAware.new(
+    constructor: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19) -> T,
+): T = constructor(
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance()
+)
+
+/**
+ * @see new
+ */
+public inline fun <
+        T,
+        reified P1,
+        reified P2,
+        reified P3,
+        reified P4,
+        reified P5,
+        reified P6,
+        reified P7,
+        reified P8,
+        reified P9,
+        reified P10,
+        reified P11,
+        reified P12,
+        reified P13,
+        reified P14,
+        reified P15,
+        reified P16,
+        reified P17,
+        reified P18,
+        reified P19,
+        reified P20
+        > DirectDIAware.new(
+    constructor: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20) -> T,
+): T = constructor(
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance(),
+    instance()
+)
