4a913ec6100d12a4f1ab72ab7e4cbd1646f86a3d
Romain BOISSELLE <romain.boisselle@proton.me>
2025-09-23 14:37:07 +0200

    chore: Kotlin 2.2.20 and update dependencies in libs.versions.toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         maven(url = "https://raw.githubusercontent.com/kosi-libs/kodein-internal-gradle-plugin/mvn-repo")
     }
     dependencies {
-        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:8.15.1")
+        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:8.16.0")
     }
 }
 
